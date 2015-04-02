139411ce5d743912f3bef18505d9cc0312790bb5
Ando Paju <andopaju@gmail.com>
2015-04-02 17:05:47 +0300

    Springi projekt

---
+++ tudeng/src/main/java/tudeng/GreetingController.java
@@ -0,0 +1,25 @@
+package tudeng;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public ModelAndView greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("otsing", "message", message);
+        //return new Greeting(counter.incrementAndGet(),
+          //                  String.format(template, name));
+    }
+}
